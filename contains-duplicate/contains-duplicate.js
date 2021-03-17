5a112022ca2f57c03f5d48c44a9f662a9c64234a
kensuke sakama <kensuke.sakama@gmail.com>
2021-03-16 20:05:42 -0700

    Time: 84 ms (87.30%), Space: 44.9 MB (60.10%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let newSet = new Set (nums);
+    if (newSet.size === nums.length) {
+        return false;
+    } else {
+        return true;
+    }
+};
