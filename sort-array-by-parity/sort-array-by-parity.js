aab7f4b67ce91c12287d008dfc7d019fdf660ea2
kensuke sakama <kensuke.sakama@gmail.com>
2021-04-09 09:27:07 -0700

    Time: 96 ms (72.94%), Space: 41.9 MB (26.41%) - LeetHub

---
+++ sort-array-by-parity/sort-array-by-parity.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function(A) {
+    
+    let even = []
+    let odd = [];
+    
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] % 2 === 0) {
+            even.push(A[i])
+        } else {
+            odd.push(A[i])
+        }
+    }
+    
+    return even.concat(odd);
+    
+};
