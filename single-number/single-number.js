dc7daf4a5903f6676ac0e62214e0b916512d9daf
kensuke sakama <kensuke.sakama@gmail.com>
2021-04-14 11:32:01 -0700

    Time: 92 ms (61.41%), Space: 42.1 MB (56.06%) - LeetHub

--- single-number/single-number.js
+++ single-number/single-number.js
@@ -3,20 +3,31 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let storage = {};
+    // let storage = {};
     nums.sort((a,b) => a-b);
     
-    for (let i = 0; i < nums.length; i++) {
-        if (storage[nums[i]] === undefined) {
-            storage[nums[i]] = 1;
-        } else {
-            storage[nums[i]]++;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (storage[nums[i]] === undefined) {
+    //         storage[nums[i]] = 1;
+    //     } else {
+    //         storage[nums[i]]++;
+    //     }
+    // }
+    // for (let key in storage) {
+    //     if (storage[key] === 1) {
+    //         return Number(key);
+    //     }
+    // }
+    // return null;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (i == 1 && nums[i-1] !== nums[i]) {
+            return nums[i-1]
         }
+        
+        if (nums[i-1] !== nums[i] && nums[i] !== nums[i+1] ) {
+            return nums[i];
+        } 
     }
-    for (let key in storage) {
-        if (storage[key] === 1) {
-            return Number(key);
-        }
-    }
-    return null;
+    return nums[nums.length-1];
 };
