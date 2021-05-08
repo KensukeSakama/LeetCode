871e596328507289162e323c39d79ea04d0a0644
kensuke sakama <kensuke.sakama@gmail.com>
2021-05-07 19:43:43 -0700

    Time: 80 ms (88.26%), Space: 39 MB (90.80%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -6,15 +6,24 @@ var maxSubArray = function(nums) {
     
     if (nums.length === 0) return 0;
     if (nums.length === 1) return nums[0];
-    let max = -Infinity;
+//     let max = -Infinity;
     
-    for (let i = 0; i < nums.length; i++) {
-        let temp = 0;
-        for (let j = i; j < nums.length; j++) {
-            temp += nums[j];
-            max = Math.max(max, temp);
-        }
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         let temp = 0;
+//         for (let j = i; j < nums.length; j++) {
+//             temp += nums[j];
+//             max = Math.max(max, temp);
+//         }
         
+//     }
+    
+    let max = -Infinity;
+    let prev = 0;
+    for (let i = 0; i < nums.length; i++) {
+        prev = Math.max(prev + nums[i], nums[i])
+        max = Math.max(max, prev);
     }
+    
     return max;
 };
