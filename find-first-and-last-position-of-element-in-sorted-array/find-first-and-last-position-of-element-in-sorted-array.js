a150655974f86ff8677fca52043eb5c8eb87ff32
kensuke sakama <kensuke.sakama@gmail.com>
2021-06-07 12:47:30 -0700

    Time: 64 ms (99.84%), Space: 39.6 MB (64.27%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -6,12 +6,8 @@
 var searchRange = function(nums, target) {
     let output = [-1, -1];
     
-    if (nums.length === 0) return [-1, -1];
-    
-    let left = 0;
-    let right = nums.length -1;
-    
-    if (nums[left] === target && nums[right] === target) return [left, right]
+    if (nums.length === 0) return [-1, -1];    
+    if (nums[0] === target && nums[nums.length-1] === target) return [0, nums.length-1]
     
     const helper = (nums, target, left, right) => {
         
@@ -23,6 +19,7 @@ var searchRange = function(nums, target) {
             let l = mid;
             let r = mid;
             while (nums[l] !== undefined || nums[r] !== undefined) {
+                if (nums[l] !== target && nums[r] !== target) break;
                 if (nums[l] === target) output[0] = l;
                 if (nums[r] === target) output[1] = r;
                 l--;
@@ -38,7 +35,7 @@ var searchRange = function(nums, target) {
         }
     }
     
-    helper(nums, target, left, right);
+    helper(nums, target, 0, nums.length-1);
     return output;
 
 };
