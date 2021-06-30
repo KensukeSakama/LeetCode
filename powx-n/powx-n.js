07ff7389673ef112385704308fd678197d9126ba
kensuke sakama <kensuke.sakama@gmail.com>
2021-06-30 15:45:52 -0700

    Time: 76 ms (84.49%), Space: 39.7 MB (40.41%) - LeetHub

---
+++ powx-n/powx-n.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    //edge cases: 
+//     x = 0
+//     n = 0
+//     n is negative
+    
+    return x**n
+    
+    
+};
